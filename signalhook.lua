1f84cd7ba2fd54c07c50b2656a3ef6be754e3099
Matrice1337 <96532892+Matrice1337@users.noreply.github.com>
2021-12-22 06:57:35 -0700

    Update signalhook.lua

--- signalhook.lua
+++ signalhook.lua
@@ -1,28 +1,53 @@
-if getgenv().raw then
-    return getgenv().raw
+if getgenv().hooksignal then
+	return getgenv().hooksignal
 end
 
-getgenv().raw = {}
+getgenv().conlib = loadstring(game:HttpGet("https://pastebin.com/raw/AyYfjrTB"))() -- my awesome connection library
+getgenv().raw = loadstring(game:HttpGet("https://pastebin.com/raw/aVDqbsU2"))() -- my awesome raw library
 
-do
-	local rawgett;rawgett = hookmetamethod(game,"__index",newcclosure(function(...)return rawgett(...)end))
-	getgenv().raw.get = rawgett
-end
-do
-	local rawsett;rawsett = hookmetamethod(game,"__newindex",newcclosure(function(...)return rawsett(...)end))
-	getgenv().raw.set = rawsett
-end
-do
-	local rawcall;rawcall = hookmetamethod(game,"__namecall",newcclosure(function(...)return rawcall(...)end))
-	getgenv().raw.call = rawcall
-end
-do
-	local rawsignal;rawsignal = hookmetamethod(raw.get(game, "Changed"), "__index", newcclosure(function(...)return rawsignal(...)end))
-	getgenv().raw.signal = rawsignal
-end
-do
-	local rawconnect;rawconnect = hookmetamethod(raw.signal(raw.get(game, "Changed"), "Connect")(raw.get(game, "Changed"), function()end), "__index", newcclosure(function(...)return rawconnect(...)end))
-	getgenv().raw.connect = rawconnect
+local signalinfo
+getgenv().getsignalinfo = function()
+	return signalinfo
 end
 
-return getgenv().raw
+local HookedSignals = {}
+
+getgenv().hooksignal = function(Signal, Function)
+	assert(typeof(Signal) == "RBXScriptSignal", string.format("Invalid argument #1 (RBXScriptSignal expected, got %s)", typeof(Signal)))
+	assert(type(Function) == "function", string.format("Invalid argument #2 (function expected, got %s)", typeof(Function)))
+
+	local Signals = conlib.getcallingsignal(Signal)
+	Signals.Functions[#Signals.Functions+1] = Function
+
+	if not HookedSignals[Signal] then
+		HookedSignals[Signal] = true
+
+		conlib.connectionadded(Signal, function(ConTbl)
+			raw.connect(ConTbl.Connection, "Disconnect")(ConTbl.Connection)
+		end)
+		for _,v in pairs(conlib.getconnections(Signal)) do
+			raw.connect(v.Connection, "Disconnect")(v.Connection)
+		end
+
+		raw.signal(Signal, "Connect")(Signal, newcclosure(function(...)
+			for Index,Connection in pairs(conlib.getconnections(Signal)) do
+				signalinfo = {Connection = Connection.Connection, Function = Connection.Function, Script = Connection.Script, Index = Index}
+				local Args = {...}
+				task.spawn(newcclosure(function()
+					for i = #Signals.Functions, 1, -1 do
+						local success,_ = pcall(function()
+							Args = {Signals.Functions[i](unpack(Args))}
+						end)
+						if not success then
+							return
+						end
+					end
+
+					Connection.Function(unpack(Args))
+				end))
+			end
+		end))
+	end
+end
+
+return getgenv().hooksignal
